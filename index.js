5cf1ed23daa92e07fd22f61e9f74289feb4b564c
Keertana Kota <keerko2218@gmail.com>
2018-06-25 23:04:33 -0400

    Homework Additions w/ Sumbit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 console.log("It Works!")
-console.warn("Hello")
+
+const button = document.querySelector('button')
+
+const up = function (){
+    const p = document.querySelector("#changed")
+    p.textContent = document.querySelector("#submit").value
+}
+
+button.addEventListener('click', up)
+
+//console.warn("Hello")
