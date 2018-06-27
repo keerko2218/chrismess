8319c58b356063bcb11d6b5742626d7b518f47e8
Keertana Kota <keerko2218@gmail.com>
2018-06-27 16:56:55 -0400

    Added arrays and objects and more buttons

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 const form = document.querySelector("#movieForm")
 
+const nameArray =[]
+
 function createSpan(){
    return document.createElement('span')
 }
@@ -16,24 +18,57 @@ function updateMovies(event){
    
     const mName = createSpan();
     const mYear = createSpan();
-
+    //const mTotal = createSpan();
+    
     item.style.color = "blue"
     mName.style.color ="purple"
     mYear.style.color = "DarkSlateBlue"
 
+    movieName.color = "blue"
+    
     mName.textContent = movieName + " "
     mYear.textContent = movieYear
+
+    const nameObject = {
+        movie:  mName.textContent,
+        year:   mYear.textContent
+    }
+    
+
+    nameArray.push(nameObject);
+
+    //mTotal.textContent = mName + mYear;
+
+    //nameArray.push(mTotal);
 
     item.appendChild(mName)
     item.appendChild(mYear)
 
     const list = document.querySelector("#movies")
     list.appendChild(item)
+
+    
+/*
+    const del = document.createElement("button")
+    list.appendChild(del)
+    del.textContent = "Delete"
+    del.name = "delete"
+    const fav = document.createElement("button")
+    fav.textContent = "Favorite this movie"
+    list.appendChild(fav)
+*/
     
     f.reset();
 }
 
+function favMovie(event){
+    event.preventDefault();
+    const p = event.target
+    updateMovies();
+}
+
 form.addEventListener('submit', updateMovies)
+form.addEventListener('fav', favMovie)
 
 
 
