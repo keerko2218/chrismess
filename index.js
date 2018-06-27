e0aebf344206788ed6f259232c0b29723cc0efee
Keertana Kota <keerko2218@gmail.com>
2018-06-27 00:12:05 -0400

    Displays the movie and year in separate fields (messy version)

--- index.js
+++ index.js
@@ -4,14 +4,25 @@ const form = document.querySelector("#movieForm")
 
 function up(event){
     event.preventDefault();
-    const movieName = document.querySelector("#submit").value 
-    const clear =  document.querySelector("#submit") 
+    const f = event.target
+    const movieName = f.movieName.value
+    const movieYear = f.movieYear.value 
+    //const clear =  document.querySelector("#submit") 
     const item = document.createElement('li')
-    item.textContent = movieName
+    const mName = document.createElement('span')
+    const mYear = document.createElement('span')
+    //item.textContent = movieName + " " + movieYear
+    mName.textContent = movieName + " "
+    mYear.textContent = movieYear
+
+
+    item.appendChild(mName)
+    item.appendChild(mYear)
 
     const list = document.querySelector("#movies")
     list.appendChild(item)
-
+    
+    f.reset();
 }
 
 form.addEventListener('submit', up)
